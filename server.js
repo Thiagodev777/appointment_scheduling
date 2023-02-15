1a2e72b484e72c9519b0a66adb4f15f7a2ca6e02
Antonio Thiago <antoniobackenddeveloper@gmail.com>
2023-02-14 21:33:26 -0300

    server settings and routes

---
+++ server.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+const app = express();
+
+import patientRoute from "./src/routes/patient.route.js";
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use("/patient", patientRoute);
+
+app.use((req, res) => {
+  return res.status(404).json({ message: "route not found" });
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
